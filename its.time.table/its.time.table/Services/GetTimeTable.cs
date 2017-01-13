dc2f08c8e3e1f177ece92ee3476e7748a02d770a
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-01-13 22:54:08 +0100

    its.time.table: Remove testings
    Remove testing Classes

--- its.time.table/its.time.table/Services/GetTimeTable.cs
+++ its.time.table/its.time.table/Services/GetTimeTable.cs
@@ -11,26 +11,30 @@ namespace its.time.table.Services
     {
         public GetTimeTable_Service()
         {
-            XWebRequest request = XWebRequest.CreateXWebRequest("https://www2.its-stuttgart.de/intranet/schueler/untis/c/47/c00024.htm");
+            var request = WebRequest.Create("https://www2.its-stuttgart.de/intranet/schueler/untis/c/47/c00024.htm");
             request.ContentType = "text/html; charset=UTF-8";
             request.Method = "GET";
             request.PreAuthenticate = true;
             request.Headers.Add("Authorization",
                 $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes("firstname.lastname:Password"))}");
 
-
-            using (var response = request.GetResponse())
+            WebResponse response = null;
+            try
             {
-                if (response.StatusCode != HttpStatusCode.OK)
-                    Console.WriteLine("Error");
-
-                using (var reader = new StreamReader(response.GetResponseStream()))
-                {
-                    var a = WebUtility.HtmlDecode(reader.ReadToEnd());
-                }
+                response = request.GetResponse();
+            }
+            catch (WebException)
+            {
+                return;
             }
 
-            
+
+            using (var reader = new StreamReader(response.GetResponseStream()))
+            {
+                var a = WebUtility.HtmlDecode(reader.ReadToEnd());
+            }
+
+
 
         }
     }
