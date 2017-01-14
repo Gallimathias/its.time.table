de8aa013f408cf6020eba505dbb1f21b0ff4506c
Marcus Aurelius <Marcus.Aurelius@gallimathias.de>
2017-01-14 18:40:27 +0100

    TestConsole: Add classes from navBar

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -18,7 +18,12 @@ namespace TestConsole
             //TimeTables: https://www2.its-stuttgart.de/intranet/schueler/untis/c/02/c00024.htm
             //Die Time Tables sind folgender masen nach /untis aufgebaut: untis/abfragetyp/Woche/typSelectedindex.htm
             //Die Informationen lassen sich aus der NavBar entnehmen.
+            GetNavBar();
+            
+        }
 
+        static void TimeTables()
+        {
             var request = WebRequest.Create("https://www2.its-stuttgart.de/intranet/schueler/untis/c/02/c00024.htm");
             request.ContentType = "text/html; charset=UTF-8";
             request.Method = "GET";
@@ -38,9 +43,38 @@ namespace TestConsole
 
 
             Deserializer.Deserailze(response.GetResponseStream());
+        }
+        
+        static void GetNavBar()
+        {
+            var request = WebRequest.Create("https://www2.its-stuttgart.de/intranet/schueler/untis/frames/navbar.htm");
+            request.ContentType = "text/html; charset=UTF-8";
+            request.Method = "GET";
+            request.PreAuthenticate = true;
+            request.Headers.Add("Authorization",
+                $"Basic {Convert.ToBase64String(Encoding.UTF8.GetBytes("vorname.name:Password"))}");
 
+            WebResponse response = null;
+            try
+            {
+                response = request.GetResponse();
+            }
+            catch (WebException)
+            {
+                return;
+            }
 
-            
+            var doc = new HtmlDocument();
+            doc.Load(response.GetResponseStream());
+            var navBar = doc.DocumentNode.Element("html").Element("head").Elements("script").ToArray()[1].InnerText.Split(
+                new[] { '\n', '\r'}, StringSplitOptions.RemoveEmptyEntries);
+            var classes = navBar.FirstOrDefault(v => v.StartsWith(" var classes")).Substring(15).TrimStart('[').TrimEnd(']').Split(',');
+            var classesDic = new Dictionary<int, string>();
+
+            for (int i = 0; i < classes.Length; i++)
+            {
+                classesDic.Add(i + 1, classes[i]);
+            }
         }
     }
 }
